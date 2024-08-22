87a8f51d647c8a346803e0822b452eda5ba0c46c
Roger Ngo <rngo@netflix.com>
2024-08-21 20:31:37 -0700

    More configuration

--- init.lua
+++ init.lua
@@ -578,7 +578,8 @@ require('lazy').setup({
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        javascript = { { 'prettierd', 'prettier' } },
+        typescript = { 'prettier' },
       },
     },
   },
