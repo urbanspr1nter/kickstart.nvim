7a94a124cf94cb17c003ca5c11adfff8cf6287a5
Roger Ngo <urbanspr1nter@gmail.com>
2024-11-20 20:58:56 -0800

    Cycling through buffers

--- init.lua
+++ init.lua
@@ -109,6 +109,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<C-7>', ':bprevious<CR>', { desc = 'Cycle to next buffer' })
+vim.keymap.set('n', '<C-8>', ':bnext<CR>', { desc = 'Cycle to previous buffer' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
